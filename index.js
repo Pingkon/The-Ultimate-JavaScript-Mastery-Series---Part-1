ff532fa12a3386dd2a6956910d5c49f49fb32238
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2022-09-19 12:47:40 +0600

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// // Arithmetic Operators
+// // // Arithmetic Operators
 
 // let x = 10;
 // let y = 3;
@@ -10,27 +10,25 @@
 // console.log(x % y);
 // console.log(x ** y);
 
-//Increment (++)
-
+// //Increment (++)
 // console.log(x);
-// console.log(++x); // increment first
+// console.log(++x); // incremented first
 // console.log(x);
 
 // console.log(x);
 // console.log(x++); // incremented later
 // console.log(x);
 
-//Decrement (--)
-
+// //Decrement (--)
 // console.log(x);
-// console.log(--x); // decrement first
+// console.log(--x); // decremented first
 // console.log(x);
 
 // console.log(x);
 // console.log(x--); // decremented later
 // console.log(x);
 
-// // Assignment Operators
+// // // Assignment Operators
 
 // let x = 10;
 
@@ -40,4 +38,47 @@
 // x = x * 3;
 // x *= 3;
 
-// // Comparison Operators
+// // // Comparison Operators
+
+// let x = 1;
+
+// // Relational
+// console.log (x>0);
+// console.log (x>=0);
+// console.log (x<1);
+// console.log (x<=1);
+
+// // Equality
+// console.log(x === 1);
+// console.log(x !== 1);
+
+// // // Ternary Operator
+
+// // If a customer has more than 100 points, they are a 'gold' customer, otherwise, they are a 'silver' customer.
+
+// let points = 110;
+// let type = points > 100 ? 'gold' : 'silver';
+
+// console.log(typle);
+
+// // // Logical Operators with Non-booleans
+
+// Falsy (false)
+// undefined
+// null
+// 0
+// ''
+// NaN
+
+// // Anything that is not Falsy -> Truthy
+
+// console.log(false || true); // returns true
+// console.log(false || 'Mosh'); //returns 'Mosh'
+// console.log(false || 1); // returns 1
+// console.log(false || 1 || 2); // returns 1 (Short-circuiting)
+
+// let userColor = '';
+// let defaultColor = 'blue';
+// let currentColor = userColor || defaultColor;
+
+// console.log(currentColor);
