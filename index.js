eafd8ae529d007b4e8c2d2dd0db994668e743436
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2022-09-19 12:18:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// // Arithmetic Operators
+
+// let x = 10;
+// let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+//Increment (++)
+
+// console.log(x);
+// console.log(++x); // increment first
+// console.log(x);
+
+// console.log(x);
+// console.log(x++); // incremented later
+// console.log(x);
+
+//Decrement (--)
+
+// console.log(x);
+// console.log(--x); // decrement first
+// console.log(x);
+
+// console.log(x);
+// console.log(x--); // decremented later
+// console.log(x);
+
+// // Assignment Operators
+
+// let x = 10;
+
+// x = x + 5;
+// x += 5;
+
+// x = x * 3;
+// x *= 3;
+
+// // Comparison Operators
