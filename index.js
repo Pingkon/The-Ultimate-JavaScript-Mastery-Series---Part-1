5e1ff6e1637fdfca0902cdc4d09da9a003278bad
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2022-09-21 12:29:05 +0600

    Exercise 4

--- index.js
+++ index.js
@@ -429,3 +429,28 @@
 // }
 
 // -------------------------------------------------
+
+// // // Exercise 4- Demerit Points
+
+// // Speed Limit = 70
+// // 5 -> 1 point
+// // Math.floor(1.3)
+// // 12 points -> suspended
+
+// function checkSpeed(speed) {
+//     const speedLimit = 70;
+//     const kmPerPoint = 5;
+//     if (speed < speedLimit + kmPerPoint)
+//         console.log('Ok')
+//     else {
+//         const points = Math.floor((speed - speedLimit) / kmPerPoint);
+//         if(points >= 12)
+//             console.log('License suspended');
+//         else
+//             console.log('Points', points);
+//     }
+// }
+
+// checkSpeed(130);
+
+// -------------------------------------------------
