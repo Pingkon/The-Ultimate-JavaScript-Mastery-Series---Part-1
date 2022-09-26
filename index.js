6601ba28e6c1513184c9312c8321cd56397b2b62
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2022-09-27 00:42:11 +0600

    Exercise 8

--- index.js
+++ index.js
@@ -504,3 +504,20 @@
 // }
 
 // -------------------------------------------------
+
+// // // Exercise 8- Sum of Multiples of 3 and 5
+
+// console.log(sum(10))
+
+// function sum(limit) {
+//     let count=0;
+
+//     for (let i=0; i<=limit; i++)
+//     if (i%3 === 0 || i%5===0) {
+//         count = count + i;
+//     }
+
+//     return count;
+// }
+
+// -------------------------------------------------
